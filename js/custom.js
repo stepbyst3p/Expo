3438839869afe1cdf2381099e2c3005d6fd6bb95
stepbyst3p <spikerdn@gmail.com>
2016-05-26 19:32:58 +0500

    Added children to sidemenu & dropdown

--- js/custom.js
+++ js/custom.js
@@ -41,13 +41,30 @@ $(document).ready(function () {
 //SideMenu
 $('.side-menu ul li a').click(function () {
     var submenu = $(this).siblings('ul');
+    submenu.toggleClass('open')
     if (submenu.length > 0) {
         if (submenu.hasClass('open')) {
 
         } else {
-            submenu.addClass('open').slideDown(1);
+
             return false;
         }
     }
 
 });
+
+var side_menu = $('.side-menu');
+var node = side_menu[0].getElementsByClassName('active')[0];
+$(node).parent().addClass('active');
+var end = false;
+if (node != undefined)
+    while (end == false) {
+        if (node.tagName == 'UL') {
+            $(node).toggleClass('open');
+
+        }
+        node = node.parentNode;
+        if (node == undefined || node.className == 'slide-menu') {
+            end = true;
+        }
+    }
